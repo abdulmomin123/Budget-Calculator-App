f3aa9eadb4de487986f629f6a46b28849384476d
Abdul Momin <abdulmomin114477@gmail.com>
2020-07-15 18:39:12 +0600

    all

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var budgetController = (function () {
   a = 10;
 
   function addNumbers(a, b) {
-    console.log(a + b);
+    return a + b;
   }
 
   return {
@@ -10,4 +10,19 @@ var budgetController = (function () {
   };
 })();
 
-budgetController.publicAddNumbers(a, 10);
+var UIController = (function () {
+  // Code goes here
+})();
+
+var controller = (function (budgetCTRL, UICTRL) {
+  // Code goes here
+  var h1 = budgetCTRL.publicAddNumbers(a, 10);
+
+  return {
+    public: function () {
+      console.log(h1);
+    },
+  };
+})(budgetController, UIController);
+
+controller.public();
