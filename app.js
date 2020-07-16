bd7623d4699019d59979c28f81cd16f61fae4e52
Abdul Momin <abdulmomin114477@gmail.com>
2020-07-16 06:29:50 +0600

    all

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
+// Budget Controller Module
 var budgetController = (function () {
-  a = 10;
-
-  function addNumbers(a, b) {
-    return a + b;
-  }
-
-  return {
-    publicAddNumbers: addNumbers,
-  };
+  // Code goes here
 })();
 
+// UI Controller Module
 var UIController = (function () {
   // Code goes here
 })();
 
+// General App Controller Module
 var controller = (function (budgetCTRL, UICTRL) {
-  // Code goes here
-  var h1 = budgetCTRL.publicAddNumbers(a, 10);
+  var addBtn = document.querySelector(".add__btn");
 
-  return {
-    public: function () {
-      console.log(h1);
-    },
+  var ctrlAddItem = function () {
+    // Get data from input fields
+    // Add item to budget controller
+    // Add item to UI
+    // Calculate the budget
+    // Display the budget
   };
-})(budgetController, UIController);
+  addBtn.addEventListener("click", ctrlAddItem);
 
-controller.public();
+  // The enter event
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
