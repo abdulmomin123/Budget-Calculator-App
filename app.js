d3f14c17339993bfbff30826f8307ca3e2b63a86
Abdul Momin <abdulmomin114477@gmail.com>
2020-07-18 06:55:32 +0600

    all

--- app.js
+++ app.js
@@ -62,9 +62,15 @@ var UIController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     inputBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
   };
 
   return {
+    getDOMStrings: function () {
+      return DOMStrings;
+    },
+
     getInput: function () {
       return {
         type: document.querySelector(DOMStrings.inputType).value, // inc or exp
@@ -73,8 +79,27 @@ var UIController = (function () {
       };
     },
 
-    getDOMStrings: function () {
-      return DOMStrings;
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+
+      // Create an HTML string with placeholder text
+      if (type === "inc") {
+        element = DOMStrings.incomeList;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        element = DOMStrings.expenseList;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      // Replace the placeholder text with object data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      // Insert HTML to the dom
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
   };
 })();
@@ -107,6 +132,7 @@ var controller = (function (budgetCTRL, UICTRL) {
     newItem = budgetCTRL.addItem(input.type, input.description, input.value);
 
     // Add item to UI
+    UICTRL.addListItem(newItem, input.type);
 
     // Calculate the budget
 
